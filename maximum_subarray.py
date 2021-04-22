44c41235767db99cee07def637998ebdcb7aab04
willyg40 <wg40@protonmail.com>
2021-04-21 20:03:52 -0500

    Improved pylint score on max subarray

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,4 +1,4 @@
-"""Implementation of divide and conquer solution to maximum-subarray 
+"""Implementation of divide and conquer solution to maximum-subarray
 problem"""
 
 def find_maximum_subarray(lst):
@@ -6,14 +6,14 @@ def find_maximum_subarray(lst):
     Uses a divide and conquer approach.
 
     Runtime: O(N * log(N))
-    
+
     Args:
         lst: A list of numeric values.
-    
-    Returns: 
-        A tuple of three integers representing the starting index, the 
-        ending index, and the sum of all of the values in the 
-        found subarray. 
+
+    Returns:
+        A tuple of three integers representing the starting index, the
+        ending index, and the sum of all of the values in the
+        found subarray.
     '''
 
     def find_max_crossing_subarray(lst, low, mid, high):
@@ -34,28 +34,28 @@ def find_maximum_subarray(lst):
                 right_sum = curr_sum
                 max_right = i
         return (max_left, max_right, left_sum + right_sum)
- 
+
     def find_max_helper(lst, low, high):
         if low == high:
             return (low, high, lst[low])
-        else:
-            mid = (low + high) // 2
-            l = find_max_helper(lst, low, mid)
-            r = find_max_helper(lst, mid + 1, high)
-            c = find_max_crossing_subarray(lst, low, mid, high)
-            if l[2] >= r[2] and l[2] >= c[2]:
-                return l
-            elif r[2] >= c[2] and r[2] >= l[2]:
-                return r
-            else:
-                return c
+        mid = (low + high) // 2
+        left = find_max_helper(lst, low, mid)
+        right = find_max_helper(lst, mid + 1, high)
+        cross = find_max_crossing_subarray(lst, low, mid, high)
+        if left[2] >= right[2] and left[2] >= cross[2]:
+            return left
+        if right[2] >= cross[2] and right[2] >= left[2]:
+            return right
+        return cross
 
     return find_max_helper(lst, 0, len(lst) - 1)
 
 
 def main():
+    """Main function for executing tests on the find_maximum_subarray function.
+    """
     lst = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-    print(find_maximum_subarray(lst))    	    	
+    print(find_maximum_subarray(lst))
 
 
 if __name__ == '__main__':
