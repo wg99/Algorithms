1cc8222cd317bd6b9a314f5b7b984ccf712ffb43
willyg40 <wg40@protonmail.com>
2021-04-26 13:55:02 -0500

    Max-heapify function

--- heap.py
+++ heap.py
@@ -1,9 +1,6 @@
 class Heap:
 
-    def __init__(self, array):
-        self.array = array
-
-    def parent(self, i): 
+    def parent(self, i):
         return (i - 1) // 2
     
     def left(self, i):
@@ -12,9 +9,21 @@ class Heap:
     def right(self, i):
         return 2 * i + 2
 
+    def max_heapify(self, A, i):
+        l = self.left(i)
+        r = self.right(i)
+        largest = i
+        if l < len(A) and A[l] > A[largest]:
+            largest = l
+        if r < len(A) and A[r] > A[largest]:
+            largest = r
+        if largest != i:
+            A[i], A[largest] = A[largest], A[i]
+            self.max_heapify(A, largest)
+
 
 if __name__ == '__main__':
-    hp = Heap([16,14,10,8,7,9,3,2,4,1])
-    print(hp.array)
-    print(hp.parent(2))
-    print(hp.array[hp.right(3)])
+    hp = Heap()
+    A = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+    hp.max_heapify(A, 1)
+    print(A)
