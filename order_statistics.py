fcc020d3811541dbf8c0e168fdde550e039abc8d
willyg40 <wg40@protonmail.com>
2021-05-12 19:24:41 -0500

    Finished function to get Nth higest number from list.

--- order_statistics.py
+++ order_statistics.py
@@ -20,13 +20,46 @@ def minimum(lst):
     return c_min
 
 
+def randomized_select(lst, i):
+    def partition(left, right):
+        pivot = lst[right]
+        i = left - 1
+        for j in range(left, right):
+            if lst[j] <= pivot:
+                i += 1
+                lst[j], lst[i] = lst[i], lst[j]
+        lst[i + 1], lst[right] = lst[right], lst[i + 1]
+        return i + 1
+
+    def randomized_partition(left, right):
+        i = random.randint(left, right)
+        lst[i], lst[right] = lst[right], lst[i]
+        return partition(left, right)
+
+    def randomized_select_helper(left, right, i):
+        if left == right:
+            return lst[left]
+        pivot = randomized_partition(left, right)
+        k = pivot - left + 1
+        if i == k:
+            return lst[pivot]
+        elif i < k:
+            return randomized_select_helper(left, pivot - 1, i)
+        else:
+            return randomized_select_helper(pivot + 1, right, i - k)
+
+    return randomized_select_helper(0, len(lst) - 1, i)
+
+
 def main():
     """Main function for running tests
-    of statistics functions.
+    of order statistics functions.
     """
-    lst = [random.randint(0, 1000) for i in range(200)]
-    print(minimum(lst))
-    print(min(lst) == minimum(lst))
+    lst = [random.randint(0, 1000) for i in range(20)]
+    print(sorted(lst))
+    print(randomized_select(lst, 1), min(lst))
+    print(min(lst) == randomized_select(lst, 1))
+    print(randomized_select(lst, 14), sorted(lst)[13])
 
 
 if __name__ == "__main__":
